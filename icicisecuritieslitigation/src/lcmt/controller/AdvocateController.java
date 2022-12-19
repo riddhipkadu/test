87c4314d4d802d9d987d527584775d1eaf37a8d1
Lex Care <Lex Care@DESKTOP-VCI3CLF>
2022-12-19 15:45:54 +0530

    Merge branch 'master' of https://github.com/akshaypatkar/Jar1.0.git

--- icicisecuritieslitigation/src/lcmt/controller/AdvocateController.java
+++ icicisecuritieslitigation/src/lcmt/controller/AdvocateController.java
@@ -33,7 +33,7 @@ public class AdvocateController {
 	@Autowired
 	EntityMappingService entityMappingService;
 
-	// Method Created : Tejashri Zurunge
+	// Method Created :Akash JAdhav
 	// Method Purpose : Add Advocate
 	@RequestMapping(value = "/addAdvocate", method = RequestMethod.GET)
 	public ModelAndView addAdvocate() {
